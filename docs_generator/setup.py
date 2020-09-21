06918e6210d57bf390235d5e2c74823dea90fd84
sirily11 <sirily1997@gmail.com>
2020-09-21 23:46:27 +0800

    fixed bugs

--- docs_generator/setup.py
+++ docs_generator/setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='docs_generator',
     version='0.1.1',
-    packages=[
+    install_requires=[
         'jupyter',
         'mkdocs',
         'pyyaml',
         'mkdocs-material',
         'pytest'
     ],
+    packages=['docs_generator'],
     url='https://github.com/sirily11/algorithms',
     license='',
     author='sirily11',
