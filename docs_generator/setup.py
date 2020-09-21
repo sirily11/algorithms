97a630e413b7c3a0a2cdf4dd9a987a0c31901999
sirily11 <sirily1997@gmail.com>
2020-09-21 23:55:41 +0800

    fixed import problem

--- docs_generator/setup.py
+++ docs_generator/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='docs_generator',
-    version='0.1.1',
+    version='0.1.2',
     install_requires=[
         'jupyter',
         'mkdocs',
